18eaba2a016472e50a2f5472eccb440ea26eac00
moiz-05 <muhammad.moiz@xray-lab.com>
2026-05-11 12:34:31 +0500

    Added requirements.txt

--- task-2/check.py
+++ task-2/check.py
@@ -5,3 +5,15 @@ import mmdet
 print(mmcv.__version__)
 print(mmengine.__version__)
 print(mmdet.__version__)
+
+# import numpy
+# import torch
+# import mmcv
+
+# print("NumPy:", numpy.__version__)
+# print("Torch:", torch.__version__)
+# print("MMCV:", mmcv.__version__)
+
+# from mmcv.ops import roi_align
+
+# print("MMCV OPS WORKING")
